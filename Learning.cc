5c165bd61f7d874b1da3665af4acdffc60b117b7
omid2007hope <omidhope2007@gmail.com>
2026-07-05 17:36:11 +0200

    Refactor output statements and add build configuration for C++ development

--- Learning.cc
+++ Learning.cc
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int myAge = 16; 
+
+double myHeight = 6.2;
+
+string myName  = "Omid";
+string mylastname = "Teimory";
+
+char bloodIdentifier = 'B';
+char bloodBar = '+';
+
+bool isAlive = true; 
+
 int main() {
-    cout << "This is C++";
+    cout << "This is" << endl << myName << endl << mylastname << "\n\n";
+    cout << "He is" << endl << myAge << endl << "years old" << "\n\n";
+    cout << "He has the height of" << endl << myHeight << "\n\n";
+    cout << "Is he alive" << endl  << isAlive << endl  << "and has the blood type of" << endl << bloodIdentifier + bloodBar << "\n\n";
     return 0;
 }
