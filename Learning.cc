a1c0a8a8c2c3c6f0c3c623e27184f6f73d42a28c
omid2007hope <omidhope2007@gmail.com>
2026-07-05 17:42:15 +0200

    Refactor output statements for clarity and add VSCode settings for C++ development

--- Learning.cc
+++ Learning.cc
@@ -14,9 +14,9 @@ char bloodBar = '+';
 bool isAlive = true; 
 
 int main() {
-    cout << "This is" << endl << myName << endl << mylastname << "\n\n";
-    cout << "He is" << endl << myAge << endl << "years old" << "\n\n";
-    cout << "He has the height of" << endl << myHeight << "\n\n";
-    cout << "Is he alive" << endl  << isAlive << endl  << "and has the blood type of" << endl << bloodIdentifier + bloodBar << "\n\n";
+    cout << "This is" << " " << myName << " " << mylastname << "\n\n";
+    cout << "He is" << " " << myAge << " " << "years old" << "\n\n";
+    cout << "He has the height of" << " " << myHeight << "\n\n";
+    cout << "Is he alive" << " " << isAlive << " " << "and has the blood type of" << " " << bloodIdentifier + bloodBar << "\n\n";
     return 0;
 }
