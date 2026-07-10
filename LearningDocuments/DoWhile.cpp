079a53b9220ae3ddb93da3fcbf3fe1cf5226c5f1
omid2007hope <omidhope2007@gmail.com>
2026-07-10 13:53:42 +0200

    Refactor DoWhile.cpp to improve product loading logic and enhance clarity

--- LearningDocuments/DoWhile.cpp
+++ LearningDocuments/DoWhile.cpp
@@ -3,6 +3,10 @@
 bool loaded = false;
 bool loading = false;
 
+int limit = 20;
+int notLoaded = 0;
+const int alreadyLoaded = 0;
+
 int main()
 {
 
@@ -12,6 +16,14 @@ int main()
 
         while (loading == true)
         {
+            if (loaded == false && alreadyLoaded == 0)
+            {
+                while (notLoaded <= limit)
+                {
+                    ++notLoaded;
+                    std::cout << "Product number: " << notLoaded << " loaded succesfully" << "\n\n";
+                }
+            }
 
             std::cout << "All 20 products loaded succesfully" << "\n\n";
             loading = false;
