6dbc63d56f487f04cfe6ac6a290b46f280ba9d17
omid2007hope <omidhope2007@gmail.com>
2026-07-11 04:46:35 +0200

    Refactor forEach.cpp to enhance output clarity and include string iteration

--- LearningDocuments/forEach.cpp
+++ LearningDocuments/forEach.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 int nums[3] = {3, 6, 9};
+std::string sample = "XYZ";
 
 int main()
 {
 
     for (int num : nums)
     {
-
         std::cout << log(num) << "\n\n";
+
+        for (char x : sample)
+        {
+            std::cout << x << "\n\n";
+        };
     };
 
     return 0;
